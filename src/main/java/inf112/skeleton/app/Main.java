6a04fdb265d720507b32e82ed7d551442b2d48a2
Mikal Drivenes <Mikal.Drivenes@student.uib.no>
2022-02-14 12:55:29 +0100

    Changed name of project to chess-game

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 public class Main {
     public static void main(String[] args) {
         Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
-        cfg.setTitle("hello-world");
+        cfg.setTitle("Chess Game");
         cfg.setWindowedMode(480, 320);
 
         new Lwjgl3Application(new HelloWorld(), cfg);
