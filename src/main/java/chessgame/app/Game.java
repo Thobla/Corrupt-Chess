9e0cf035e547ad6e86fbeabfc389d970140649eb
emila <emila@10.111.4.129>
2022-02-18 12:11:36 +0100

    Added Utils into the right branch

--- src/main/java/chessgame/app/Game.java
+++ src/main/java/chessgame/app/Game.java
@@ -7,13 +7,10 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector3;
-
 import chessgame.entities.Player;
+import chessgame.utils.CameraStyles;
 import chessgame.world.GameMap;
-import chessgame.world.TileType;
 import chessgame.world.TiledGameMap;
-import com.badlogic.gdx.physics.box2d.*;
 
 public class Game implements ApplicationListener {
     OrthographicCamera cam;
@@ -26,7 +23,7 @@ public class Game implements ApplicationListener {
     	
     	//PlayerController
     	playerController = new PlayerController();
-    	
+
         //The camera viewpoint
         cam = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         cam.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
@@ -49,7 +46,7 @@ public class Game implements ApplicationListener {
     @Override
     public void dispose() {
     	batch.dispose();
-    	gameMap.dispose();
+       gameMap.dispose();
     }
 
     @Override
@@ -64,21 +61,21 @@ public class Game implements ApplicationListener {
     	player.getSprite().setPosition(player.getPosition().x, player.getPosition().y);
     	batch.end();
     	
-        cam.position.set(player.getSprite().getX(), player.getSprite().getY(), 0);
-        cam.update();
+    	CameraStyles.lockOnTarget(cam, player.getPosition());
+    	
+    	
+//        cam.position.x += (player.getPosition().x + 20 - cam.position.x) * 0.5 * 4;
+//        cam.position.y = player.getPosition().y;
+//        cam.update();
+   
+
+        
+        
+//        cam.position.set(player.getSprite().getX(), player.getSprite().getY(), 0);
+//        cam.update();
         
         //Camera within bounds
         cameraBounds();
-        
-        /*
-        //For tile testing
-        if (Gdx.input.justTouched()) {
-        	Vector3 pos = cam.unproject(new Vector3 (Gdx.input.getX(), Gdx.input.getY(), 0));
-        	TileType type = gameMap.getTileTypeByLocation(0, pos.x, pos.y);
-        	if(type != null)
-        		System.out.println(type.name());
-       
-        }*/
     }
 
     @Override
---
+++ src/main/java/chessgame/utils/CameraStyles.java
@@ -0,0 +1,18 @@
+package chessgame.utils;
+
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
+
+public class CameraStyles {
+
+    public static void lockOnTarget(Camera camera, Vector2 target) {
+        Vector3 position = camera.position;
+        position.x = target.x;
+        position.y = target.y;
+        camera.position.set(position);
+        camera.update();
+    }
+}
