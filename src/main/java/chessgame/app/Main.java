367e973297578c9a184ae4ca9c44d1506d043a1f
Asmundur of the western mountains <asmundalmenning@gmail.com>
2022-03-03 20:42:48 +0100

    Oppdaterte oppløysning

--- src/main/java/chessgame/app/Main.java
+++ src/main/java/chessgame/app/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
         cfg.setTitle("Chess Game");
-        cfg.setWindowedMode(1500, 1000);
+        cfg.setWindowedMode(1980, 1000);
         //cfg.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
         ChessGame game = new ChessGame();
         new Lwjgl3Application(game, cfg);
