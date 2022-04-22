232cf5f0a5601b54fb922c7e8e3ae07873eaf4ca
Mikal Drivenes <Mikal.Drivenes@student.uib.no>
2022-04-22 13:29:43 +0200

    Fixed some formatting and imports

--- src/main/java/chessgame/menues/LevelSelectScreen.java
+++ src/main/java/chessgame/menues/LevelSelectScreen.java
@@ -82,25 +82,17 @@ public class LevelSelectScreen implements Screen{
 
 	@Override
 	public void pause() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void resume() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void hide() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
-		
 	}
 }
--- src/main/java/chessgame/world/PhysicsWorld.java
+++ src/main/java/chessgame/world/PhysicsWorld.java
@@ -1,8 +1,5 @@
 package chessgame.world;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.MapObjects;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
@@ -12,9 +9,9 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Fixture;
-import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.Shape;
+import com.badlogic.gdx.physics.box2d.World;
 
 import chessgame.entities.Button;
 import chessgame.entities.Door;
